49572537dbd107b682cbb65ab17e217ccb13f188
Kamatchipriya25 <priyaasekar25@gmail.com>
2025-01-25 08:40:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/project-webnox/",
+  base: "/webnox-reactproject/",
   plugins: [react()],
 })
